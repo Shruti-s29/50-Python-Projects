11795af8bbebd6015db31ed145602f6645891845
Shruti Shastri <shruti24ai057@satiengg.in>
2023-01-20 02:35:06 +0530

    project - 11 (2 files) 	new file:   11.music_player.py 	new file:   Audio_sample.mp3

---
+++ 11.music_player.py
@@ -0,0 +1,8 @@
+# For using this module you have to externally install it through -
+# pip install playsound on terminal
+
+# playsound module has a playsound method/function in it.
+from playsound import playsound 
+
+# pass full path to the audio file as argument
+playsound("Audio_sample.mp3")
